871aff8b85038b44941afe09ff64120cb5f2d4bf
Subhransu <subhranshumajhi2@gmail.com>
2023-06-16 01:01:21 +0530

    add about me page

---
+++ src/pages/about.js
@@ -0,0 +1,50 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import profilePic from "../../public/images/profile/profile-pic.png";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Subhransu | About page</title>
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-4 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                About Me
+              </h2>
+              <p className="font-medium">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget
+                sit amet tellus cras adipiscing enim.
+              </p>
+              <p className="my-4 font-medium">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget
+                sit amet tellus cras adipiscing enim.
+              </p>
+              <p className="font-medium">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget
+                sit amet tellus cras adipiscing enim.
+              </p>
+            </div>
+            <div className="col-span-1"></div>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+              <Image src={profilePic} alt="" className="w-full rounded-2xl" />
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
