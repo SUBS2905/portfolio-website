15a197a64d0e0302ab0d96ad3209bd7e07cc73c7
Subhransu <subhranshumajhi2@gmail.com>
2023-06-15 22:18:19 +0530

    add homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
-import Head from 'next/head'
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import profilePic from "../../public/images/profile/developer-profile.jpg";
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -6,9 +12,48 @@ export default function Home() {
       <Head>
         <title>Portfolio Website</title>
       </Head>
-      <main className=''>
-        Homepage
+      <main className="flex items-center text-dark w-full min-h-screen">
+        <Layout className="pt-8">
+          <div className="flex justify-between items-center w-full">
+            <div className="w-1/2">
+              <Image
+                src={profilePic}
+                alt=""
+                className="w-3/4 h-auto rounded-lg"
+              />
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="text-left"
+              />
+              <p className="my-4 text-base font-medium">
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js and
+                web development.
+              </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/resume.pdf"
+                  target={"_blank"}
+                  download={true}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark "
+                >
+                  Resume <LinkArrow className={"w-6 m-1"} />
+                </Link>
+                <Link
+                  href="mailto:subhransu.majhi2020@vitbhopal.ac.in"
+                  target={"_blank"}
+                  className="ml-4 text-lg font-medium capitalize underline "
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
-  )
+  );
 }
